d172b867431208af09923de301c4f28e715beee6
Dawood Khoja <114805991+Dawood006@users.noreply.github.com>
2025-04-04 15:08:49 +0530

    Update nb_model.py

--- nb_model.py
+++ nb_model.py
@@ -99,23 +99,22 @@ if st.button("🔍 Check for Spam!"):
     else:
         with st.spinner("PuppyGuard is sniffing your text... 🐾"):
             prediction = predict_spam(user_input)
-
-            # Inside the spam detection block
-        if prediction == 1:
-        st.error("""
-        🚨 *WOOF WOOF! SPAM ALERT!* 🚨  
-        PuppyGuard detected malicious content!  
-        Better not open this one! 🦴
-        """)
-        st.image("https://img.icons8.com/color/452/dog-angry.png", width=120)  # angry dog
-        else:
-        st.success("""
-        🎉 *YAY! NO SPAM DETECTED!* 🎉  
-        PuppyGuard gives this email a paws-up!  
-        It looks safe to open! 🐶
-        """)
-        st.image("https://img.icons8.com/color/452/dog.png", width=120)  # happy dog
-        st.balloons()
+            
+            if prediction == 1:
+            st.error("""
+            🚨 *WOOF WOOF! SPAM ALERT!* 🚨  
+            PuppyGuard detected malicious content!  
+            Better not open this one! 🦴
+            """)
+            st.image("https://img.icons8.com/color/452/dog-angry.png", width=120)  # angry dog
+            else:
+            st.success("""
+            🎉 *YAY! NO SPAM DETECTED!* 🎉  
+            PuppyGuard gives this email a paws-up!  
+            It looks safe to open! 🐶
+            """)
+            st.image("https://img.icons8.com/color/452/dog.png", width=120)  # happy dog
+            st.balloons()
 
 # Footer
 st.markdown("---")
